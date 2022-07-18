07e1e8714e8683b3100acc79705c8c44c078d2a9
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-07-18 16:38:44 -0300

    add routes and authprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home";
+import SignUp from "components/SignUp";
+import Login from "components/Login";
 
 function App() {
   return (
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
   );
 }
 
